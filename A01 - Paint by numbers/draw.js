756f30f7f383b1ce7be00d88c9e12ed0cfe16a8c
Daniele Marsella <daniele.marsella95@gmail.com>
2019-05-06 20:23:26 +0200

    Added first assignement solution
    Signed-off-by: Daniele Marsella <daniele.marsella95@gmail.com>

---
+++ A01 - Paint by numbers/draw.js
@@ -0,0 +1,13 @@
+function draw() {
+	// line(x1,y1, x2,y2)
+	// draws a line from a point at Normalized screen coordinates x1,y1 to Normalized screen coordinates x2,y2
+	line(0.5, 0.5, 0.0, 0.8);
+	line(0.0, 0.8,-0.5, 0.5);
+	line(-0.5, 0.5, -0.5, -0.7);
+	line(-0.5, -0.7, 0.5, -0.7);
+	line(0.5, -0.7, 0.5, 0.5);
+	line(0.2, -0.7, 0.2, -0.1);
+	line(-0.2, -0.1, 0.2, -0.1);
+	line(-0.2, -0.1, -0.2, -0.7);
+}
+
