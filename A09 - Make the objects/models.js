f0ee4307a6d91bba0c99a5f062c0f870c7848e64
Daniele Marsella <daniele.marsella95@gmail.com>
2019-05-27 20:38:34 +0200

    Added assignement 09 and started 10
    Signed-off-by: Daniele Marsella <daniele.marsella95@gmail.com>

---
+++ A09 - Make the objects/models.js
@@ -0,0 +1,175 @@
+function buildGeometry() {
+    var i;
+    var i2;
+    
+    // Draws a pyramid
+    var vert1 = [[-1.0,-1.0,0.0], [1.0,-1.0,0.0], [0.0,1.0,1],
+		 [-1, -1.0, 2], [1, -1, 2]];
+    var ind1 = [0, 2, 1,
+		0, 1, 4,
+		0, 4, 3,
+		1, 2, 4,
+		3, 2, 0,
+		4, 2, 3
+	       ];
+    var color1 = [1.0, 0.0, 0.0];
+    addMesh(vert1, ind1, color1);
+
+    // Draws a cube
+    var vert2 = [[-1.0,-1.0,0.0], [1.0,-1.0,0.0], [-1.0,1.0,0.0], [1.0,1.0,0.0],
+		 [-1.0,-1,2], [1,-1,2], [-1,1,2],[1,1,2]]
+    var ind2 = [0, 2, 1, 1, 2, 3,
+		1, 3, 5, 5, 3, 7,
+		0, 4, 2, 4, 6, 2,
+		1, 5, 0, 5, 4, 0,
+		2, 6, 7, 3, 2, 7,
+		5, 7, 4, 6, 4, 7
+	       ];
+    var color2 = [0.0, 0.0, 1.0];
+    addMesh(vert2, ind2, color2);
+
+    // Draws a Monopoly house
+    var vert3 = [[-1.0,-1.0,-1], [1.0,-1.0,-1], [1.0,1.0,-1], [-1.0,1.0,-1],
+		 [0.0,2, 0],
+		 [-1.0,-1.0,1], [1.0,-1.0,1], [1.0,1.0,1], [-1.0,1.0, 1]];
+    var ind3 = [0, 2, 1,
+		0, 3, 2,
+		3, 4, 2, // front side
+		1, 2, 7,
+		1, 7, 6,
+		2, 4, 7,
+		5, 8, 0,
+		8, 3, 0,
+		8, 4, 3,// left and right sides
+		1, 6, 0,
+		6, 5, 0, // bottom side
+		6, 7, 5,
+		7, 8, 5,
+		7, 4, 8, // back side		
+	       ];
+    var color3 = [0.0, 1.0, 0.0];
+    addMesh(vert3, ind3, color3);
+    
+    // Draws a Cone
+    var vert4 = [[0.0, 0.0, 0.0]];
+    var ind4 = [];
+    var color4 = [1.0, 1.0, 0.0];
+    var slices4 = 10;
+    for(i = 0; i < slices4; i++) {
+	vert4[i+1] = [Math.cos(2*Math.PI / slices4 * i), 0.0 ,Math.sin(2*Math.PI / slices4 * i) ];
+	ind4[3*i]   = 0;
+	ind4[3*i+1] = i+1;
+	ind4[3*i+2] = (i < slices4-1) ? i+2 : 1 ;
+    }
+    i++;
+    vert4[i] = [0, 2, 0];
+    for (i2=0; i2< slices4; i2++) {	
+    	ind4[3*i2 + 3*i ]    = (i2 < slices4-1) ? i2 + 2 : 1 ;
+    	ind4[3*i2 + 3*i + 1] =  i2 + 1;
+    	ind4[3*i2 + 3*i + 2] = i;
+    }
+    addMesh(vert4, ind4, color4);
+
+    // Draws a Cylinder
+    var vert5 = [[0.0, -1, 0.0], [0,1,0]];
+    var ind5 = [];
+    var color5 = [1.0, 0.0, 1.0];
+    var slices5 = 64;
+    for(i = 0; i < slices5*2; i=i+2) {
+	// bottom circle
+	vert5[i+2] = [Math.cos(2*Math.PI / slices5 * (i/2)), -1, Math.sin(2*Math.PI / slices5 * (i/2))];
+	ind5[12*i/2]   = 0;
+	ind5[12*i/2 +1] = i+2;
+	ind5[12*i/2 +2] = (i < slices5*2-3) ? i+4 : 2 ;
+	// side
+	ind5[12*i/2 + 3]   = i+2 ;
+	ind5[12*i/2 + 4]   = i+3 ;
+	ind5[12*i/2 + 5]   = (i < slices5*2-3) ? i+4 : 2 ;
+	ind5[12*i/2 + 6]   = i+3;
+	ind5[12*i/2 + 7]   = (i < slices5*2-4) ? i+5 : 3 ;
+	ind5[12*i/2 + 8]   = (i < slices5*2-3) ? i+4 : 2 ;
+	//top circle
+	vert5[i+3] = [Math.cos(2*Math.PI / slices5 * (i/2)), 1, Math.sin(2*Math.PI / slices5 * (i/2))];
+	ind5[12*i/2 + 9]   = (i < slices5*2-4) ? i+5 : 3; // swapped because back-face culling
+	ind5[12*i/2 + 10] = i+3;
+	ind5[12*i/2 + 11] = 1 ;
+
+    }	
+    addMesh(vert5, ind5, color5);
+
+    // Draws a Sphere
+    var vert6 = [[0,0,0]];
+    var ind6 = [];
+    var color6 = [0.0, 1.0, 1.0];
+    var slices6 = 50;
+    var strips6 = 20;
+    var ray6 = 2;
+    vert6[1] = [0, ray6,0];
+    var currentRay = ray6 * Math.cos(Math.PI/2 - Math.PI / strips6 * 1); 
+    var currentHeight = ray6 * Math.sin(Math.PI/2 - Math.PI/strips6 * 1 );
+    var prevStartVertexIndex = 2;
+    var fillVertexIndex = 2; // from  which we can start to add vertices
+    var fillIndexIndex = 0; // from which we can start to add indexes
+    for(i = 0; i < slices6; i++) {
+	// bottom
+	vert6[i+fillVertexIndex] = [currentRay * Math.cos(2*Math.PI / slices6 * i), currentHeight , currentRay * Math.sin(2*Math.PI / slices6 * i)];
+	ind6[fillIndexIndex + 3*i]   = 1;
+	ind6[fillIndexIndex + 3*i + 1] = i+fillVertexIndex;
+	ind6[fillIndexIndex + 3*i + 2] = (i < slices6 - 1) ? i+fillVertexIndex+1 : fillVertexIndex ;
+    }    
+    fillIndexIndex = i*3; // added index for this cycle
+    prevStartVertexIndex = fillVertexIndex ; // store where the previous circle start
+    fillVertexIndex = fillVertexIndex + i; // added vertices for this cycle
+    var j;
+    for (j = 1; j < strips6-1; j++) {
+    	i2 = 0;
+    	currentRay = ray6 * Math.cos(Math.PI/2 -  Math.PI / strips6 * (j+1) ); 
+    	currentHeight = ray6 *  Math.sin(Math.PI/2 - Math.PI/strips6 * (j+1) );
+    	while( i2 < slices6 ) {	
+    	    vert6[fillVertexIndex + i2 ] = [currentRay * Math.cos(2*Math.PI / slices6 * i2), currentHeight , currentRay * Math.sin(2*Math.PI / slices6 * i2)];
+    	    ind6[fillIndexIndex + 6*i2    ] = prevStartVertexIndex + i2;
+    	    ind6[fillIndexIndex + 6*i2 + 1] = fillVertexIndex + i2 ;
+    	    ind6[fillIndexIndex + 6*i2 + 2] = (i2 < slices6 - 1) ? prevStartVertexIndex + 1 + i2 : prevStartVertexIndex ;
+    	    ind6[fillIndexIndex + 6*i2 + 3] =  fillVertexIndex + i2 ;
+    	    ind6[fillIndexIndex + 6*i2 + 4] =  (i2 < slices6 - 1) ? fillVertexIndex + i2 + 1   : fillVertexIndex ;
+    	    ind6[fillIndexIndex + 6*i2 + 5] = (i2 < slices6 - 1) ? prevStartVertexIndex + 1 + i2 : prevStartVertexIndex ;
+    	    i2++;
+    	}
+    	prevStartVertexIndex = fillVertexIndex;
+    	fillVertexIndex = fillVertexIndex + i2;
+    	fillIndexIndex = fillIndexIndex + i2 * 6;
+    }
+    vert6[fillVertexIndex]= [0,-ray6,0];
+    fillVertexIndex++;
+    currentRay = ray6 * Math.cos(-Math.PI/2 + Math.PI / strips6 ); 
+    currentHeight = ray6 * Math.sin(-Math.PI/2 + Math.PI/strips6);
+    for(i = 0; i < slices6; i++) {
+	// bottom
+	vert6[i+fillVertexIndex] = [currentRay * Math.cos(2*Math.PI / slices6 * i), currentHeight , currentRay * Math.sin(2*Math.PI / slices6 * i)];
+	ind6[fillIndexIndex + 3*i]   = (i < slices6 - 1) ? i+fillVertexIndex+1 : fillVertexIndex;
+	ind6[fillIndexIndex + 3*i + 1] = i+fillVertexIndex;
+	ind6[fillIndexIndex + 3*i + 2] = fillVertexIndex-1 ;
+    }    
+    // side strips
+    // i2 = 0;
+    // var j = 1;
+    // while( j < strips6 - 1) {
+
+    // 	j++;
+    // }
+
+    // //top 
+
+    // i2 = 0;
+    // while( i2 < slices6) {
+    // 	// bottom
+    // 	var currentRay = ray6 * Math.sin(2* Math.PI / strips6); 
+    // 	vert6[i + i2] = [Math.cos(2*Math.PI / slices6 * i), ray6 *  Math.cos(Math.PI - 2*Math.PI/strips6 ), Math.sin(2*Math.PI / slices6 * i)];
+    // 	ind6[i + 3*i2]   = i;
+    // 	ind6[i + 3*i2 + 1] = i+i2+1;
+    // 	ind6[i + 3*i2 + 2] =  (i2 < slices6 - 1) ? i+i2+2 : i+1;
+    // 	i2++;
+    // }    
+    addMesh(vert6, ind6, color6);
+}
+
---
+++ A10 - Shaders/shaders.js
@@ -0,0 +1,87 @@
+function shaders() {
+// The shader can find the required informations in the following variables:
+
+//vec3 fs_pos;			// Position of the point in 3D space
+//
+//vec3 LAPos;			// Position of first (or single) light
+//vec3 LADir;			// Direction of first (or single) light
+//float LAConeOut;		// Outer cone (in degree) of the light (if spot)
+//float LAConeIn;		// Inner cone (in percentage of the outher cone) of the light (if spot)
+//float LADecay;		// Decay factor (0, 1 or 2)
+//float LATarget;		// Target distance
+//vec4 LAlightColor;	// color of the first light
+//		
+//vec3 LBPos;			// Same as above, but for the second light
+//vec3 LBDir;
+//float LBConeOut;
+//float LBConeIn;
+//float LBDecay;
+//float LBTarget;
+//vec4 LBlightColor;
+//
+//vec3 LCPos;			// Same as above, but for the third one
+//vec3 LCDir;
+//float LCConeOut;
+//float LCConeIn;
+//float LCDecay;
+//float LCTarget;
+//vec4 LClightColor;
+//
+//vec4 ambientLightColor;		// Ambient light color. For hemispheric, this is the color on the top
+//vec4 ambientLightLowColor;	// For hemispheric ambient, this is the bottom color
+//vec3 ADir;					// For hemispheric ambient, this is the up direction
+//
+//float SpecShine;				// specular coefficient for both blinn and phong
+//float DToonTh;				// Threshold for diffuse in a toon shader
+//float SToonTh;				// Threshold for specular in a toon shader
+//
+//vec4 diffColor;				// diffuse color
+//vec4 ambColor;				// material ambient color
+//vec4 specularColor;			// specular color
+//vec4 emit;					// emitted color
+//	
+//vec3 normalVec;				// direction of the normal vecotr to the surface
+//vec3 eyedirVec;				// looking direction
+//
+//
+// Final color is returned into:
+//vec4 out_color;
+
+// Single directional light, Lambert diffuse only: no specular, no ambient, no emission
+var S1 = `
+        float cosAlphaAngle = dot(normalVec, LADir);
+	out_color = LAlightColor *  diffColor * clamp(cosAlphaAngle, 0.0, 1.0);
+`;
+
+// Single point light with decay, Lambert diffuse, Blinn specular, no ambient and no emission
+var S2 = `
+	out_color = vec4(0.0, 1.0, 0.0, 1.0);
+`;
+
+// Single directional light, Lambert diffuse, Phong specular, constant ambient and emission
+var S3 = `
+	out_color = vec4(1.0, 1.0, 0.0, 1.0);
+`;
+
+// Single spot light (with decay), Lambert diffuse, Blinn specular, no ambient and no emission
+var S4 = `
+	out_color = vec4(0.0, 0.0, 1.0, 1.0);
+`;
+
+// Single directional light, Cartoon diffuse, Cartoon specular, no ambient but emission
+var S5 = `
+	out_color = vec4(1.0, 0.0, 1.0, 1.0);
+`;
+
+// Single directional light, no diffuse, phong specular, hemispheric ambient and no emission
+var S6 = `
+	out_color = vec4(0.0, 1.0, 1.0, 1.0);
+`;
+
+// Three lights: a directional, a point and a spot. Lambert diffuse, phong specular, constant ambient and no emission
+var S7 = `
+	out_color = vec4(1.0, 1.0, 1.0, 1.0);
+`;
+	return [S1, S2, S3, S4, S5, S6, S7];
+}
+
