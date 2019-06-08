42462b0b2ef348d16bcd159ff70a810022839a2f
Generoso Imparato <10478628@polimi.it>
2019-06-08 12:22:22 +0200

    Fixed health/energy bar

--- engine/InterfaceOverlay.js
+++ engine/InterfaceOverlay.js
@@ -2,26 +2,34 @@ var InterfaceOverlay =
 {
 	init()
 	{
-		this.healthBarOverlay = new Box3D(0.2, 0.5, 0, new TextureMaterial("health_bar.png"));
-		this.healthBarOverlay.setPosition(-0.7, -0.7, -0.1);
-		this.healthBarOverlay.setRotation(0, 0, 90);
-		this.healthBar = new Box3D(0.35, 0.65, 0, new SimpleMaterial(255, 0, 0, 150));
-		this.healthBar.setPosition(0.22, 0.08, 0);
-		//this.healthBar.setRotation(0, 0, -90);
-		this.healthBar.setParent(this.healthBarOverlay);
+		this.bar = new Box3D(0.25, 0.5, 0, new TextureMaterial("health_energy_bar.png"));
+		this.bar.setPosition(-0.7, -0.7, -0.1);
+		this.bar.setRotation(0, 0, 90);
 
-		/*this.energyBar = new Box3D(0.3, 0.05, 0, new SimpleMaterial(0, 100, 255, 150));
-		this.energyBar.setPosition(-0.65, -0.8, 0);*/
+		//health bar
+		this.healthBar = new Box3D(0.32, 0.8, 0, new SimpleMaterial(255, 0, 0, 150));
+		this.healthBar.setPosition(-0.20, 0.07, 0);
+		this.healthBar.setParent(this.bar);
+
+		//energy bar
+		this.energyBar = new Box3D(0.32, 0.8, 0, new SimpleMaterial(10, 100, 255, 150));
+		this.energyBar.setPosition(0.21, 0.07, 0);
+		this.energyBar.setParent(this.bar);
 	},
 
 	render()
 	{
-		//parallel
+		//parallel projection
 		viewMatrix = utils.identityMatrix();
 		projectionMatrix = utils.identityMatrix();
 
+		//update health and energy from player
+		this.healthBar.setScale(0.32, 0.8*player.health, 0);
+		this.energyBar.setScale(0.32, 0.8*player.energy, 0);
+
 		//this.energyBar.render();
 		this.healthBar.render();
-		this.healthBarOverlay.render();
+		this.energyBar.render();
+		this.bar.render();
 	}
 }
--- engine/Player.js
+++ engine/Player.js
@@ -14,15 +14,31 @@ class Player extends GroupObject3D
 		this.setScale(2, 2, 2);
 
 		this.moveSpeed = standardMoveSpeed;
+		this.health = 1.0;
+		this.energy = 1.0;
 	}
 
 	handleInput()
 	{
 		//sprint
-		if(Input.isKeyDown(Input.SHIFT_KEY))
+		if(Input.isKeyDown(Input.SHIFT_KEY) && this.energy > 0.1)
+		{
 			this.moveSpeed = sprintMoveSpeed;
-		else
+			this.energy -= 0.015;
+		}
+		else if(Input.isKeyDown(Input.SHIFT_KEY) && this.energy > 0.0)
+		{
 			this.moveSpeed = standardMoveSpeed;
+			this.energy -= 0.015;
+		}
+		else
+		{
+			this.moveSpeed = standardMoveSpeed;
+			this.energy += 0.010;
+			if(this.energy > 1.0)	
+				this.energy = 1.0;
+		}
+
 
 		//move in the forward direction
 		if(Input.isKeyDown(Input.W_KEY))
